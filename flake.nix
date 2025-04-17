5068e499c26704ac3060adf98761006a998f0881
Asterikss <AndreAndreM@proton.me>
2025-04-17 20:02:29 +0200

    create a base quarto flake

--- flake.nix
+++ flake.nix
@@ -13,15 +13,29 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+        pkgs = import nixpkgs { inherit system; };
+
+        # Create a function that can be used by other flakes to extend Quarto
+        mkQuartoEnv =
+          {
+            extraPythonPackages ? (ps: [ ]),
+          }:
+          pkgs.quarto.override {
+            extraPythonPackages = ps: extraPythonPackages ps;
+          };
       in
       {
+        # Export the function for other flakes to use
+        lib = {
+          inherit mkQuartoEnv;
+        };
+
         devShells.default =
           with pkgs;
           mkShell {
-            packages = [ quarto ];
+            packages = [
+              quarto
+            ];
           };
       }
     );
